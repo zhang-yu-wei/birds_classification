755a15e8d4796ade078595729c228fdb1b006362
zyuwei <1611515@mail.nankai.edu.cn>
2019-08-16 20:14:12 -0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import net
+from utils import ImageGenerator
+
+batch_size = 2
+epochs = 10
+learning_rate = 0.0001
+
+gen = ImageGenerator(batch_size, True, './data')
+
+
+with tf.device('/cpu:0'):
+    sess = tf.Session()
+
+    images = tf.placeholder(tf.float32, [batch_size, 480, 480, 3])
+    true_out = tf.placeholder(tf.float32, [batch_size, 4])
+    train_mode = tf.placeholder(tf.bool)
+
+    net = net.Vgg16()
+    net.build(images, train_mode)
+
+    sess.run(tf.global_variables_initializer())
+
+    cost = tf.reduce_sum((net.prob - true_out)**2)
+    train = tf.train.GradientDescentOptimizer(0.0001).minimize(cost)
+
+    for epoch in range(epochs):
+        batch, img_result = gen.next_batch()
+        sess.run(train, feed_dict={images: batch, true_out: img_result, train_mode: True})
+        del batch, img_result
+
+        print(str(epoch))
---
+++ utils.py
@@ -0,0 +1,61 @@
+import numpy as np
+import os
+import random
+
+
+class ImageGenerator():
+    def __init__(self, batch_size, train, path):
+        self.batch_size = batch_size
+        self.train = train # bool get what type of data
+        self.last = 0
+        self.path = path
+
+        self.train_list = [name for name in os.listdir(path + '/' + 'train') if '.npy' in name]
+        random.shuffle(self.train_list)
+
+        self.test_list = [name for name in os.listdir(path + '/' + 'test') if '.npy' in name]
+        random.shuffle(self.test_list)
+
+        self.valid_list = [name for name in os.listdir(path + '/' + 'valid') if '.npy' in name]
+        random.shuffle(self.valid_list)
+
+        self.train_num = len(self.train_list)
+        self.test_num = len(self.test_list)
+        self.valid_num = len(self.valid_list)
+
+    def next_batch(self):
+        train_last = self.train_num - 1
+
+        if self.last > train_last - self.batch_size:
+            new_size = train_last - self.last
+        else:
+            new_size = self.batch_size
+        data_batch = np.zeros([new_size, 480, 480, 3])
+        label_batch = [new_size, 4]
+        count = 0
+        while(count<new_size):
+            data_name = self.train_list[self.last]
+            data_batch[count] = np.load(self.path + '/' + 'train' + '/' + data_name)
+            label_batch[count] = [1 if i == int(data_name[6]) else 0 for i in range(4)]
+            count += 1
+            self.last += 1
+        if self.last == train_last:
+            self.last = 0
+        return data_batch, label_batch
+
+    def get_valid(self):
+        data_batch = np.zeros([self.valid_num, 480, 480, 3])
+        label_batch = [self.valid_num, 4]
+        count = 0
+        while(count<self.valid_num):
+            data_name = self.valid_list[count]
+            data_batch[count] = np.load(self.path + '/' + 'valid' + '/' + data_name)
+            label_batch[count] = [1 if i == int(data_name[6]) else 0 for i in range(4)]
+            count += 1
+
+        return data_batch, label_batch
+
+
+
+
+
